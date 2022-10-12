bec6a13ab477dac228477d06b7be367a86b17d8d
hznagui <113547201+hznagui@users.noreply.github.com>
2022-10-12 18:55:51 +0100

    Add files via upload

--- projetlewel/ft_memmove.c
+++ projetlewel/ft_memmove.c
@@ -3,43 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hznagui <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/10 18:04:12 by hznagui           #+#    #+#             */
-/*   Updated: 2022/10/10 19:23:36 by hznagui          ###   ########.fr       */
+/*   Updated: 2022/10/12 18:31:59 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
-void *ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	int i;
-	int p;
-	char *o;
-	char *u;
-	i = 0;
-	p = 0; 
-	o =(char *)dst;
-	u =(char *)src;
-	while (o[i])
+	int		i;
+	int		p;
+	char	*o;
+	char	*u;
+
+	i = 1;
+	p = 0;
+	o = (char *)dst;
+	u = (char *)src;
+	while (i <= len)
 	{
+		o[len - i] = u[len - i];
 		i++;
-	}
-	while (len < i)
-	{
-		o[i] = u[i];
-		i--;
 	}
 	return (o);
 }
-int main()
+/*int main()
 {
-	char y[15]="hello";
-	char i[15]="hello";
+	char y[15]="gregjjghjrndjk";
+	char i[15]="gregjjghjrndjk";
 	//char p[15];
 	ft_memmove(y+2,y,5);
 	memmove(i+2,i,5);
 	printf("%s\n",y);
 	printf("%s",i);
-}
+}*/
