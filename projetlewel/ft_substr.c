d94f44c8753e5cc46bf1828916a5a576f2c753ad
hznagui <113547201+hznagui@users.noreply.github.com>
2022-10-15 21:20:25 +0100

    Add files via upload

---
+++ projetlewel/ft_substr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 18:25:58 by marvin            #+#    #+#             */
+/*   Updated: 2022/10/15 18:25:58 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "ft_strlen.c"
+#include "ft_strdup.c"
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    
+    size_t o;
+    o = 0;
+    char *p = malloc(len);
+    if(ft_strlen(s) < (size_t)start)
+     ft_strdup(0);
+    if (!p)
+        return (0);
+    while (o < len)
+    {
+        p[o]=s[start + o] ;
+        o++;
+    }
+    p[o]='\0';
+    return(p);
+}
+int main()
+{
+    char l[]="hello nega";
+    printf("%s",ft_substr(l,5,0));
+}
