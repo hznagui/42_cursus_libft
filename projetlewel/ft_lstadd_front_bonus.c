039ec2ffb199a7794e6a5abce3121c98761fd169
houdayfa znagui <113547201+hznagui@users.noreply.github.com>
2022-10-28 01:16:57 +0100

    Add files via upload

--- projetlewel/ft_lstadd_front_bonus.c
+++ projetlewel/ft_lstadd_front_bonus.c
@@ -6,16 +6,16 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/25 22:18:25 by hznagui           #+#    #+#             */
-/*   Updated: 2022/10/26 18:53:32 by hznagui          ###   ########.fr       */
+/*   Updated: 2022/10/27 15:49:30 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-    if (!new || !lst)
-        return ;
-    new->next = *lst;
-    *lst = new;
+	if (!new || !lst)
+		return ;
+	new->next = *lst;
+	*lst = new;
 }
--- projetlewel/ft_lstmap_bonus.c
+++ projetlewel/ft_lstmap_bonus.c
@@ -6,12 +6,28 @@
 /*   By: hznagui <hznagui@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 16:18:57 by hznagui           #+#    #+#             */
-/*   Updated: 2022/10/27 19:07:35 by hznagui          ###   ########.fr       */
+/*   Updated: 2022/10/27 20:58:27 by hznagui          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 {
+    t_list *head = NULL;
+    t_list *new;
     
+    if(!f || !lst || !del)
+        return (0);
+    while (lst)
+    {
+        new = ft_lstnew(f(lst -> content));
+        if (!new)
+        {
+            ft_lstclear(&head,del);
+            return (0);
+        }
+        ft_lstadd_back(&head,new);
+        lst = lst->next;
+    }
+    return(head);
 }
